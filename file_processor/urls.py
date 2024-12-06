8ae5feade85718ff1d2e1c60acb6afc79408610d
PAn1COG <sushrut.diwan.sd@gmail.com>
2024-12-06 09:35:24 -0800

    update-url

--- file_processor/urls.py
+++ file_processor/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload_file, name='upload_file'),
     path('status/', views.file_status, name='file_status'),
-    path('all_tasks/', views.all_tasks, name='all_tasks'),
-    path('renderAll_tasks/', views.renderAll_tasks, name='all_tasks'),
+    path('all-tasks/', views.all_tasks, name='all_tasks'),
+    path('renderAll-tasks/', views.renderAll_tasks, name='all_tasks'),
     path('renderStatus/', views.status, name='file_status'),
-    path('delete_all_tasks/', views.delete_all_tasks, name='delete_all_tasks'),
+    path('delete-all-tasks/', views.delete_all_tasks, name='delete_all_tasks'),
 ]
