470cb251f36ea32b0a5e01dcab05e7e501cd1447
PAn1COG <sushrut.diwan.sd@gmail.com>
2024-12-07 12:33:34 -0800

    update-tests.py

--- file_processor/tests.py
+++ file_processor/tests.py
@@ -1,10 +1,11 @@
-import io
 from django.test import TestCase
 from django.urls import reverse
 from file_processor.models import FileTask 
 import uuid
+# import io
 
 class AllTasksViewTest(TestCase):
+    # SETUP
     def setUp(self):
         # Create sample FileTask objects
         FileTask.objects.create(
@@ -19,16 +20,16 @@ class AllTasksViewTest(TestCase):
             status='processing',
             result=None
         )
+        self.url = reverse('all_tasks')
 
     def test_all_tasks_returns_correct_data(self):
+        # Stimulus
         # send request
-        url = reverse('all_tasks')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
+        # Sift
         # Assert 200 status code
         self.assertEqual(response.status_code, 200)
-
-        # Assert
         expected_data = [
             {
                 'task_id': str(FileTask.objects.all()[0].task_id),
@@ -46,18 +47,16 @@ class AllTasksViewTest(TestCase):
         self.assertJSONEqual(response.content, expected_data)
 
     def test_all_tasks_returns_empty_list_if_no_tasks(self):
-        # Delete all tasks
+        
+        # Stimulus
         FileTask.objects.all().delete()
-
-        # Send request
-        url = reverse('all_tasks')
-        response = self.client.get(url)
-
-        # Assert
+        response = self.client.get(self.url)
+        # Sift
         self.assertJSONEqual(response.content, [])
 
 
 class DeleteAllTasksviewTest(TestCase):
+    # SETUP
     def setUp(self):
         # Create sample FileTask objects
         FileTask.objects.create(
@@ -72,14 +71,15 @@ class DeleteAllTasksviewTest(TestCase):
             status='processing',
             result=None
         )
+        self.url = reverse('delete_all_tasks')
         
     def test_delete_all_tasks_success(self):
+        # Stimulus
         # Send request
-        url = reverse('delete_all_tasks')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         
+        # Sift
         self.assertEqual(response.status_code, 200)
-        
         self.assertEqual(FileTask.objects.count(), 0)
         
         # expected_response = {"message": "All tasks have been successfully deleted."}
@@ -88,6 +88,7 @@ class DeleteAllTasksviewTest(TestCase):
         
         
 class FileStatusViewTest(TestCase):
+    # SETUP
     def setUp(self):
         self.task = FileTask.objects.create(
             task_id=uuid.uuid4(),
@@ -98,11 +99,12 @@ class FileStatusViewTest(TestCase):
         self.url = reverse('file_status')
     
     def test_file_status_seccess(self):
+        # Stimulus
         url = f"{self.url}?task_id={self.task.task_id}"
         response = self.client.get(url)
         
+        # sift
         self.assertEqual(response.status_code, 200)
-        
         expected_data = {
             'task_id': str(self.task.task_id),
             'file_name': 'file1.txt',
@@ -113,18 +115,19 @@ class FileStatusViewTest(TestCase):
         self.assertJSONEqual(response.content, expected_data)
         
     def test_file_status_notFound(self):
+        # Stimulus
         url = f"{self.url}?task_id={uuid.uuid4()}"
         response = self.client.get(url)
         
+        # sift
         self.assertEqual(response.status_code, 404)
-        
         self.assertJSONEqual(response.content, {'error': 'Task not found'})
         
     def test_file_status_taskId_notFound(self):
+        # Stimulus
         response = self.client.get(self.url)
-        
+        # Sift
         self.assertEqual(response.status_code, 400)
-        
         self.assertJSONEqual(response.content, {'error': 'Task id is required'})
         
     
